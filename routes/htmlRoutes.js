47b1fb96e19d43b1763bd9ccee442c55792e0f64
Duhhvonte <devonte.letts@gmail.com>
2020-12-02 18:48:28 -0500

    updated readme

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-const { dirname } = require("path");
 const path = require("path");
 
 
@@ -14,21 +12,4 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
     
-=======
-const { dirname } = require("path");
-const path = require("path");
-
-
-module.exports = function(app) {
-    // HTML GET Requests
-  
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
-    
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-    
->>>>>>> da425016f85fb5372ece4072c3d110756077a65d
 };
